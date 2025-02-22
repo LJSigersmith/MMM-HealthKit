58d3ee82c8b1e7763bca04f86059b0ee8562958c
LJSigersmith <lancesigersmith@uconn.edu>
2025-02-22 16:06:31 -0500

    debug act ring

--- MMM-HealthKit.js
+++ MMM-HealthKit.js
@@ -1,7 +1,7 @@
 // Frontend module
 // Integrates with MagicMirror
 // Displays Data
-import { createActivityRing } from "./activityRing/activityRing.js";
+const { createActivityRing } = require("./activityRing/activityRing.js");
 
 Module.register("MMM-HealthKit", {
    
