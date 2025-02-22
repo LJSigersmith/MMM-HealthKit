526f7ff0e54b18b5cf1b2625f6b8366a032faf9f
LJSigersmith <lancesigersmith@uconn.edu>
2025-02-22 15:48:24 -0500

    debug act ring

--- MMM-HealthKit.js
+++ MMM-HealthKit.js
@@ -99,8 +99,8 @@ Module.register("MMM-HealthKit", {
         wrapper.appendChild(macroBar);
 
         // ACTIVITY RINGS
-        const exerciseRingElement = createActivityRing(moveValue, moveGoal);
-        wrapper.appendChild(exerciseRingElement);
+        //const exerciseRingElement = createActivityRing(moveValue, moveGoal);
+        //wrapper.appendChild(exerciseRingElement);
 
         return wrapper;
         /*
